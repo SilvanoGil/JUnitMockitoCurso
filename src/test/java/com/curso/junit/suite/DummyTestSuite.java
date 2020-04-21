ec984df3f85006a9b3c18af9c92324fc476c5db8
sgil <sgil@pronoide.es>
2020-04-21 14:24:33 +0200

    First step
    First step

---
+++ src/test/java/com/curso/junit/suite/DummyTestSuite.java
@@ -0,0 +1,14 @@
+package com.curso.junit.suite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.curso.junit.helper.ArraysTest;
+import com.curso.junit.helper.StringHelperTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ArraysTest.class,StringHelperTest.class})
+public class DummyTestSuite {
+
+}
